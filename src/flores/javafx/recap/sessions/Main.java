fc82e5aa3d76eaa8ac58eb4268272627a59a5710
enfl333 <enrique.pflores@gmail.com>
2021-02-20 13:56:21 +0100

    first change

--- src/flores/javafx/recap/sessions/Main.java
+++ src/flores/javafx/recap/sessions/Main.java
@@ -6,6 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Adding comments.
+ * @Author enfl
+ */
 public class Main extends Application {
 
     @Override

46f0a6d3c1acf80e8e1b8a720027afe9313723f6
enfl333 <enrique.pflores@gmail.com>
2021-02-25 22:09:19 +0100

    Added Icons, Menu Items close application etc

--- src/main/java/flores/javafx/recap/sessions/Screen1Controller.java
+++ src/main/java/flores/javafx/recap/sessions/Screen1Controller.java
@@ -5,8 +5,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import org.apache.log4j.Logger;
-
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -19,13 +19,15 @@ public class Screen1Controller implements Initializable {
     @FXML
     private ListView<String> nameList;
 
-    public void processName(){
+    public void addName(){
         String name = enterNameTxTField.getText().trim();
         if(name.trim().length()>0){
             System.out.println("Getting text from the field for a Name!!!:"+name);
             Logger.getLogger(this.getClass()).debug(" ProcesName: "+name);
             enterNameTxTField.clear();
             nameList.getItems().add(name);
+            enterNameTxTField.requestFocus();
+
         }else {
             Alert alert = new Alert(Alert.AlertType.INFORMATION, "Can't enter an empty name");
             alert.show();
@@ -45,6 +47,8 @@ public class Screen1Controller implements Initializable {
                 if (response == ButtonType.OK) {
                     nameList.getItems().remove(deleteIndex);
                     nameList.refresh();
+                    nameList.getSelectionModel().clearSelection();
+
                 }
             });
         }
@@ -56,5 +60,12 @@ public class Screen1Controller implements Initializable {
        ObservableList<String> names = FXCollections.observableArrayList("Default Name");
         Locale.setDefault(Locale.ENGLISH);
         nameList.setItems(names);
+        enterNameTxTField.setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.ENTER)
+            {
+                addName();
+            }
+        });
+
     }
 }
